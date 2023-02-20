ea2581002cd993ef2b3a7a3fa6112d27b6b5ce00
Dautarim <75495689+Dautarim@users.noreply.github.com>
2023-02-20 20:54:07 -0300

    agora vamos tentar com dados differentes

--- pages/api/tempoo.js
+++ pages/api/tempoo.js
@@ -4,10 +4,38 @@ function tempo(request, response)
 {
     const dataobj = new Date()
 
-    response.json(
+    response.json([
         {
            date: dataobj.toGMTString()
+        }   
+        ,{
+            nome: "Syren De Mer",
+            linkImg:"http://img60.imagetwist.com/th/08496/j722koyc8pf7.jpg",
+            linkSite:"https://pt.pornhub.com/video/search?search=Syren+De+Mer",
+            dataDeNascimento:"13 de Junho de 1985"
         }
+        ,
+        {
+            nome: "Melissa Lynn",
+            linkImg:"https://eoquetemprahj.com/wp-content/uploads/2017/11/03-1.jpg",
+            linkSite:"https://pt.pornhub.com/video/search?search=Melissa+Lynn",
+            dataDeNascimento:"13 de Junho de 1985"
+        }
+        ,
+        {
+            nome: "Bridgette B",
+            linkImg:"https://milf.vip/pics/doctoradventures/bridgette-b/xxxbeata-milf-xxxevelin/bridgette-b-4.jpg",
+            linkSite:"https://pt.pornhub.com/video/search?search=Bridgette+B",
+            dataDeNascimento:"13 de Junho de 1985"
+        }
+        ,
+        {
+            nome: "Misty Stone",
+            linkImg:"https://static-ca-cdn.eporner.com/gallery/cW/hJ/xIOZdfAhJcW/45625-misty-stone-nude.jpg",
+            linkSite:"https://pt.pornhub.com/video/search?search=miss+raquel",
+            dataDeNascimento:"13 de Junho de 1985"
+        }
+    ]
     )
 }
 
