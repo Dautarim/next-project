e25dab63ea03af2253bba42fe5f5a615099765b9
Dautarim <75495689+Dautarim@users.noreply.github.com>
2023-02-20 15:22:56 -0300

    já esse comit na outra branch, fiquei com raiva e n vou escrever a mesma merda de novo

---
+++ pages/aboutUs.js
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+function noSobre()
+{
+    return <div>
+     <h1>
+        Hello word
+    </h1> 
+        <Link href="/">
+            Home
+        </Link>
+    </div>
+}
+
+export default noSobre
